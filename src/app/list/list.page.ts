1a1d0287400326e8fd41e6c4d41bbcd69de79805
Josh Bailey <joshuaclaytonbailey@gmail.com>
2020-04-27 20:02:54 -0700

    imported project service to list.ts

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../project.service'
 
 @Component({
   selector: 'app-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
 
-  constructor() { }
+  constructor(public _projectService: ProjectService) { }
 
   ngOnInit() {
   }
